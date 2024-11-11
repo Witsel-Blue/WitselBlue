72f6f6175b8dd5ed70a4970166b47605c74d8ae9
Witsel-Blue <witselblue@gmail.com>
2024-11-11 19:31:30 +0900

    [FEAT] Page Transition fix

--- components/PageTransition.vue
+++ components/PageTransition.vue
@@ -1,8 +1,11 @@
 <template>
     <div id="page-transition">
-        <TextMask 
-            :text="title">
-        </TextMask>
+        <div class="bg">
+            <TextMask 
+                :text="title">
+            </TextMask>
+        </div>
+        <div class="bg2"></div>
     </div>
 </template>
 
@@ -30,19 +33,19 @@
             pageTransition() {
                 setTimeout(function(){
                     document.getElementById('page-transition').classList.add('active');
-                }, 1400);
+                }, 1000);
             },
         }
     }
 </script>
 
 <style lang="scss" scoped>
-    #page-transition {
-        width: 100vw;
-        height: 100vh;
+    .bg {
+        width: 400vw;
+        height: 180vh;
         position: fixed;
-        top: 0;
-        left: 0;
+        top: -180%;
+        left: -150vw;
         z-index: 100;
         background-color: #2c2a2a;
         color: #fff;
@@ -54,12 +57,40 @@
         opacity: 1;
         visibility: visible;
         transition: all 0.4s;
+        border-radius: 50%;
+        animation: animation 2s;
+    }
+    .bg2 {
+        width: 100%;
+        height: 100%;
+        background-color: #f7f7f7;
+        position: fixed;
+        top: 0;
+        left: 0;
+        z-index: 99;
+    }
 
-        &.active {
+    #page-transition.active {
+        .bg2 {
             opacity: 0;
             visibility: hidden;
             z-index: -999;
             transition: all 0.4s;
+        }
+    }
+
+    @keyframes animation {
+        0% {
+            top: 140%;
+        }
+        20% {
+            top: -40vh;
+        }
+        80% {
+            top: -40vh;
+        }
+        100% {
+            top: -180%;
         }
     }
 </style>
