55b5cc5780903f97b75a107c5921e1666e20433e
Witsel-Blue <witselblue@gmail.com>
2025-09-02 21:09:35 +0900

    [FEAT] pager 스크롤 최하단시 go top 문구 추가

--- components/pager.vue
+++ components/pager.vue
@@ -1,10 +1,14 @@
 <template>
     <div id='pager'>
         <div class='wrapper mouse-hover1'>
+            <p v-show='showGoTop' class='gotop'>
+                click to<br>go top
+            </p>
             <img
                 :src="require('@/assets/img/pager.svg')"
                 @click='scrollToTop'
                 alt='pager'
+                class='pager'
             >
         </div>
     </div>
@@ -20,20 +24,30 @@ if (process.client) {
     
 export default {
     name: 'Pager',
+    data() {
+        return {
+            showGoTop: false,
+            scrollTween: null,
+            scrollTrigger: null,
+        }
+    },
     mounted() {
         if (process.client) {
             this.initGsap();
+            window.addEventListener('scroll', this.checkScrollBottom);
+
+            this.$watch('$route', () => {
+                this.$nextTick(() => {
+                    this.resetGsap();
+                    this.initGsap();
+                });
+            });
+
         }
     },
     beforeDestroy() {
-        if (this.scrollTween) {
-            this.scrollTween.kill();
-            this.scrollTween = null;
-        }
-        if (this.scrollTrigger) {
-            this.scrollTrigger.kill();
-            this.scrollTrigger = null;
-        }
+        this.resetGsap();
+        window.removeEventListener('scroll', this.checkScrollBottom);
     },
     methods: {
         async initGsap() {
@@ -41,21 +55,37 @@ export default {
             const { ScrollTrigger } = await import('gsap/dist/ScrollTrigger.js');
 
             gsap.registerPlugin(ScrollTrigger);
+            gsap.set('.pager', { rotation: 0 });
 
             const triggerEl = document.getElementById('app') || document.body;
 
-            this.scrollTween = gsap.to('#pager', {
-                rotation: 360*3,
+            this.scrollTween = gsap.to('.pager', {
+                rotation: 360*2,
                 ease: 'none',
                 scrollTrigger: {
-                trigger: triggerEl,
-                start: 'top top',
-                end: 'bottom bottom',
-                scrub: true,
+                    trigger: triggerEl,
+                    start: 'top top',
+                    end: 'bottom bottom',
+                    scrub: true,
                 },
             });
 
             this.scrollTrigger = this.scrollTween.scrollTrigger;
+        },
+        resetGsap() {
+            if (this.scrollTween) {
+                this.scrollTween.kill();
+                this.scrollTween = null;
+            }
+            if (this.scrollTrigger) {
+                this.scrollTrigger.kill();
+                this.scrollTrigger = null;
+            }
+            gsap.set('.pager', { rotation: 0 });
+        },
+        checkScrollBottom() {
+            const bottom = window.innerHeight + window.scrollY >= document.documentElement.scrollHeight - 1;
+            this.showGoTop = bottom;
         },
         scrollToTop() {
             if (process.client) {
@@ -84,10 +114,17 @@ export default {
             max-height: 120px;
             min-width: 80px;
             min-height: 80px;
+            .gotop {
+                position: absolute;
+                top: 0;
+                left: 50%;
+                transform: translate(-50%, -100%);
+                text-align: center;
+                width: 100%;
+            }
             img {
                 width: 100%;
                 height: 100%;
-                // cursor: pointer;
             }
         }
     }
